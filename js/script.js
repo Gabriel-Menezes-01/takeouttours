3ba5094c8d32c50c9ed3a31e1655652422f75ec1
Gabriel Menezes <gabrielme2000.gm@gmail.com>
2025-04-14 17:48:57 +0100

    troca do header, ajuste de slide

--- js/script.js
+++ js/script.js
@@ -1,9 +1,69 @@
-const btnDrop = document.querySelector('.nav-icon');
-const btnDropIcon = document.querySelector('.nav-icon i');
-const btnDropMenu = document.querySelector('.dropdown_menu');
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close')
 
-btnDrop.onclick = function (){
-    btnDropMenu.classList.toggle('open')
-    const isOpen = btnDropMenu.classList.contains('open')
-    btnDropIcon.classList = isOpen ? 'bi bi-x-lg':'bi bi-list'
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
 }
+
+/* Menu hidden */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
+
+// slider
+
+let imgSlider = document.querySelectorAll('.slide-box');
+let btnProximo = document.querySelector('#proxima');
+let btnAnterior = document.querySelector('#anterior');
+let btnNav = document.querySelectorAll('.btn-nav')
+
+let contadorImg = imgSlider.length;
+let imgAtiva = 0;
+
+btnProximo.addEventListener('click',  ()=>{
+   imgAtiva++;
+   if (imgAtiva >= contadorImg) {
+      imgAtiva = 0;
+   }
+   mostraslider();
+})
+
+btnAnterior.addEventListener('click', ()=>{
+   imgAtiva--;
+   if (imgAtiva < 0) {
+      imgAtiva = contadorImg - 1;
+   }
+   mostraslider();
+})
+
+function mostraslider() {
+   let imgAntiga = document.querySelector('.slide-conteiner .slide-box.ativo');
+   let btnAntiga = document.querySelector('.btn-nav-box .btn-nav.ativo');
+
+   imgAntiga.classList.remove('ativo');
+   btnAntiga.classList.remove('ativo');
+
+   imgSlider[imgAtiva].classList.add('ativo')
+   btnNav[imgAtiva].classList.add('ativo')
+}
+
+btnNav.forEach((btn, indice)=>{
+   btn.addEventListener('click', ()=>{
+      imgAtiva = indice;
+      mostraslider();
+   })
+});
+// passa a cada 8s
+setInterval(() => {
+   imgAtiva++;
+   if (imgAtiva >= contadorImg) {
+      imgAtiva = 0;
+   }
+   mostraslider();
+}, 8000); 
